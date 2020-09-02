4f7a285e4548e67ba16809f7139dacf541f84337
jonayoung <young.jon@icloud.com>
2020-09-01 21:24:39 -0400

    updated settings

--- BR_Server/settings.py
+++ BR_Server/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('django_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['br-server.herokuapp.com']
 
 
 # Application definition
