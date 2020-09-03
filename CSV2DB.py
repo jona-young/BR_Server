b4be1978564b44ea2e0392984d45ebbf9d8e0feb
jonayoung <young.jon@icloud.com>
2020-09-02 22:02:46 -0400

    added csv for courtinfractions.models.contactInfo database upload

--- CSV2DB.py
+++ CSV2DB.py
@@ -1,7 +1,8 @@
-import csv
+import os, csv
 from courtinfractions.models import contactInfo
+from .BR_Server.settings import BASE_DIR
 
-CSV_PATH = '/Users/jonathanyoung/Documents/Django/nameemail.csv'      # Csv file path
+CSV_PATH = '{}nameemail.csv'.format(BASE_DIR)      # Csv file path
 
 
 with open(CSV_PATH, newline='', encoding="mac_roman") as csvfile:
